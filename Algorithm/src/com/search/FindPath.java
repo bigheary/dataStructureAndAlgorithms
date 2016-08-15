1856374fbc35bf7df5ad2226b6663cf321a1ba32
xiexiaodong02 <xie.xd@foxmail.com>
2016-08-15 21:52:25 +0800

    on first return

--- Algorithm/src/com/search/FindPath.java
+++ Algorithm/src/com/search/FindPath.java
@@ -1,5 +1,8 @@
 package com.search;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * Created by xiexiaodong02 on 2016/8/14.
  */
@@ -11,6 +14,8 @@ public class FindPath {
     private static int x0, y0;
     private static int x1, y1;
     private static int[][] dt = new int[4][];
+    private static boolean found = false;
+    private static LinkedList<Node> stack = new LinkedList<Node>();
     static {
         dt[0] = new int[]{0, 1};
         dt[1] = new int[]{1, 0};
@@ -22,9 +27,8 @@ public class FindPath {
      * */
     public static void dfs(int x, int y, int step){
         if (x == x1 && y == y1){
-            if (step < min){
-                min = step;
-            }
+            found = true;
+            printStack(stack);
             return;
         }
 
@@ -37,10 +41,15 @@ public class FindPath {
                 continue;
             }
 
+            if (found)
+                return;
+
             if (arr[nx][ny] == 0 &&  visited[nx][ny] == false){
+                stack.push(new Node(nx, ny));
                 visited[nx][ny] = true;
                 dfs(nx, ny, step+1);
                 visited[nx][ny] = false;
+                stack.pop();
             }
         }
 
@@ -64,12 +73,31 @@ public class FindPath {
     }
 
     public static void main(String[] args){
-        x1 = 4;
+        x1 = 3;
         y1 = 2;
         x0 = 0;
         y0 = 0;
         init();
+        visited[0][0] = true;
         dfs(x0, y0, 0);
+    }
+
+    private static class Node{
+        int x;
+        int y;
+        Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void printStack(LinkedList<Node> stack){
+        Iterator<Node> iterator = stack.iterator();
+        while (iterator.hasNext()){
+            Node tmp = iterator.next();
+            System.out.print("("+tmp.x+", "+tmp.y+")   ");
+        }
+        System.out.println();
     }
 
 }
